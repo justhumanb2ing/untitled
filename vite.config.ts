df25fb71eb58c89657251704a3f09f7728182b1e
coggiee <zentechie7@gmail.com>
2025-12-30 20:39:00 +0900

    fix clerk middleware error on react router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { intlayer, intlayerProxy } from "vite-intlayer";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), intlayer(), intlayerProxy()],
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd());
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    build: {
+      sourcemap: config.mode === "production",
+      rollupOptions: config.isSsrBuild
+        ? { input: "./server/app.ts" }
+        : undefined,
+    },
+    plugins: [
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths(),
+      intlayer(),
+      intlayerProxy(),
+    ],
+  };
 });
