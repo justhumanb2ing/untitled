98cb267d73ec5c0cae7cf3499814feb9363548ee
coggiee <zentechie7@gmail.com>
2025-12-31 21:43:55 +0900

    Add UserButton component and integrate user authentication in home route; update loader to fetch primary handle from Supabase.

---
+++ app/components/user-button.tsx
@@ -0,0 +1,39 @@
+import { LocalizedLink } from "@/components/localized-link";
+import { Button } from "./ui/button";
+import { useUser } from "@clerk/react-router";
+import { Spinner } from "./ui/spinner";
+
+interface UserButtonProps {
+  primaryHandle: string | null;
+}
+
+export default function UserButton({ primaryHandle }: UserButtonProps) {
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) return <Spinner />;
+
+  return (
+    <Button
+      variant={"secondary"}
+      size={"lg"}
+      className="gap-0 rounded-full py-0 ps-0 pe-3 h-10 px-5"
+      render={
+        primaryHandle ? (
+          <LocalizedLink to={`/user/${primaryHandle}`} />
+        ) : undefined
+      }
+    >
+      <div className="me-1.5 flex aspect-square h-full p-1.5 relative">
+        <img
+          alt="Profile image"
+          aria-hidden="true"
+          className="h-auto w-full rounded-full"
+          height={32}
+          src={user?.imageUrl}
+          width={32}
+        />
+      </div>
+      <span>{user?.firstName + " " + user?.lastName}</span>
+    </Button>
+  );
+}
--- app/routes/($lang)._index.tsx
+++ app/routes/($lang)._index.tsx
@@ -1,14 +1,12 @@
 import LocaleSwitcher from "@/components/locale-switcher";
 import type { Route } from "./+types/($lang)._index";
 import { ThemeToggle } from "@/components/theme-toggle";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/react-router";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/react-router";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { getAuth } from "@clerk/react-router/server";
+import { getSupabaseServerClient } from "@/lib/supabase";
+import UserButton from "@/components/user-button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,15 +15,38 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+export async function loader(args: Route.LoaderArgs) {
+  const { userId } = await getAuth(args);
+
+  if (!userId) return { primaryHandle: null };
+  
+  const supabase = await getSupabaseServerClient(args);
+
+  const { data, error } = await supabase
+    .from("pages")
+    .select("handle")
+    .eq("owner_id", userId!)
+    .eq("is_primary", true)
+    .maybeSingle();
+
+  if (error || !data?.handle) {
+    return { primaryHandle: null };
+  }
+
+  return { primaryHandle: data.handle };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { primaryHandle } = loaderData;
+
   return (
     <main>
-      <header className="flex items-center justify-end mx-auto max-w-7xl py-4 gap-2 px-4">
+      <header className="flex items-center justify-end mx-auto max-w-7xl py-4 gap-2 px-2">
         <SignedOut>
           <Button render={<SignInButton />} />
         </SignedOut>
         <SignedIn>
-          <UserButton />
+          <UserButton primaryHandle={primaryHandle} />
         </SignedIn>
         <Separator orientation="vertical" />
         <nav className="flex items-center justify-end gap-2">
