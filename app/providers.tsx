f63343753bfdf9ed93726159e777972465d93ae2
coggiee <zentechie7@gmail.com>
2026-01-02 14:35:07 +0900

    add toast component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { IntlayerProvider } from "react-intlayer";
 import { useI18nHTMLAttributes } from "./hooks/use-i18n-html-attributes";
+import { AnchoredToastProvider, ToastProvider } from "@/components/ui/toast";
 
 export default function Providers({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   useI18nHTMLAttributes();
 
-  return <IntlayerProvider>{children}</IntlayerProvider>;
+  return (
+    <IntlayerProvider>
+      <ToastProvider position="bottom-center" timeout={5000}>
+        <AnchoredToastProvider>{children}</AnchoredToastProvider>
+      </ToastProvider>
+    </IntlayerProvider>
+  );
 }
