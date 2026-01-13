03e8b0e6fcac454271131da5a168b8ad27edc36b
coggiee <zentechie7@gmail.com>
2026-01-13 17:40:15 +0900

    chore

--- app/routes/($lang)._auth.onboarding.tsx
+++ app/routes/($lang)._auth.onboarding.tsx
@@ -14,7 +14,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
-import type { Route } from "./+types/$lang._auth.onboarding";
+import type { Route } from "./+types/($lang)._auth.onboarding";
 import { Button } from "@/components/ui/button";
 import {
   Form as RhfForm,
